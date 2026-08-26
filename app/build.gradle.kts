da080b68902513f0e35dfd8de38e65bcc74ccdee
nstarstar <deliciayielding046@evilbx.com>
2026-08-26 13:46:18 +0200

    Update comments in build.gradle.kts for clarity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,14 +40,14 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     
-    // UI Compose
+    // Material 3 Compose
     implementation(platform("androidx.compose:compose-bom:2023.05.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    // Sieć i API
+    // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
